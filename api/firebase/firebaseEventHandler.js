5ede46a19f9dec160d673dbecfa2486a6489ee89
duonghung1269 <hanbdi@gmail.com>
2017-08-14 18:31:46 +0000

    integrate firebase admin

--- api/firebase/firebaseEventHandler.js
+++ api/firebase/firebaseEventHandler.js
@@ -3,9 +3,18 @@
 // Import Admin SDK
 var admin = require("firebase-admin");
 
+var serviceAccount = require("./daikin-lirc-pi-firebase-adminsdk.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.DATABASE_URL
+});
+
 // Get a database reference to our posts
 var db = admin.database();
-var ref = db.ref("server/saving-data/fireblog/posts");
+var ref = db.ref("history_events");
+
+console.log("init firebase...");
 
 function startListenEvent() {
 	// Attach an asynchronous callback to read the data at our posts reference
@@ -13,6 +22,14 @@ function startListenEvent() {
 	  console.log(snapshot.val());
 	}, function (errorObject) {
 	  console.log("The read failed: " + errorObject.code);
+	});
+
+	// Retrieve new posts as they are added to our database
+	ref.on("child_added", function(snapshot, prevChildKey) {
+  		var newEvent = snapshot.val();
+  		console.log("Fan: " + newEvent.fan);
+  		//console.log("Title: " + newPost.title);
+  		//console.log("Previous Post ID: " + prevChildKey);
 	});
 }
 
